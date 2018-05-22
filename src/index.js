04f83258377dee842a98c77f50e4cfdbd00d7919
Bearsie <wojtek.niedzwiedz@gmail.com>
2018-05-22 22:08:01 +0200

    Adding testing tools. Setting up eslint for tests.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import chalk from 'chalk';
 import clear from 'clear';
 import figlet from 'figlet';
 import inquirer from 'inquirer';
-import { QUESTIONS } from './app/inquirerSetup';
+import { FILTER_OPTIONS, PALINDROME_TYPES, QUESTIONS } from './app/inquirerSetup';
 
 clear();
 
 (async () => {
   console.log(chalk.yellow(figlet.textSync('Palindrome', { horizontalLayout: 'full' })));
   console.log(chalk('This app calculates palindrome for given range of numbers.'));
+  const possibleChoices = { FILTER_OPTIONS, PALINDROME_TYPES };
   const inputedData = await inquirer.prompt(QUESTIONS);
+  console.log(possibleChoices);
   console.log(inputedData);
 })();

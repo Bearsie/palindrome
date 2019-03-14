d6d72987838fb9db6052b8968b6d8dae186f791e
Bearsie <wojtek.niedzwiedz@gmail.com>
2019-03-14 21:11:51 +0100

    Remove comented code, refactor

--- src/components/palindromes/computePalindromes.js
+++ src/components/palindromes/computePalindromes.js
@@ -1,20 +1,12 @@
 import findPalindromes from './findPalindromes';
 
-export default (data) => {
-  const {
-    rangeFrom,
-    rangeTo,
-    filtersToApply,
-    selectedPalindromeTypes,
-    calcFunction,
-  } = data;
-
-  // const reversedSelectedPalindromeTypes = selectedPalindromeTypes.reverse();
-
-  return selectedPalindromeTypes.map((palindromeType) => (
-    {
-      palindromeType,
-      palindrome: findPalindromes(palindromeType)(rangeFrom, rangeTo)(filtersToApply)(calcFunction),
-    }
-  ));
-};
+export default ({
+  rangeFrom,
+  rangeTo,
+  filtersToApply,
+  selectedPalindromeTypes,
+  calcFunction,
+}) => selectedPalindromeTypes.map((palindromeType) => ({
+  palindromeType,
+  palindrome: findPalindromes(palindromeType)(rangeFrom, rangeTo)(filtersToApply)(calcFunction),
+}));
